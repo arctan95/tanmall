25cc3d7bc6094d4444a7474e0fadeb5d0047f850
arctan95 <tanzq1995@163.com>
2020-10-12 17:35:36 +0800

    finish Nacos config

--- mall-coupon/src/main/java/com/tan/mall/coupon/controller/CouponController.java
+++ mall-coupon/src/main/java/com/tan/mall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +26,22 @@ import com.tan.common.utils.R;
  * @email tanzhq1995@gmail.com
  * @date 2020-10-10 22:20:49
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value(value = "${coupon.user.name}")
+    private String name;
+    @Value(value = "${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test() {
+        return R.ok().put("name", name).put("age",age);
+    }
 
     @RequestMapping("/member/list")
     public R memberCoupons() {
