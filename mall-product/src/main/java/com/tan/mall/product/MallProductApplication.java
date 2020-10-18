c2e04572ca7083c60e31fbe9fb1465efdfd46968
arctan95 <tanzq1995@163.com>
2020-10-18 22:36:12 +0800

    set alicloud-oss

--- mall-product/src/main/java/com/tan/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/tan/mall/product/MallProductApplication.java
@@ -8,11 +8,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * @description 1、整合MyBatis-Plus
  *                  1）导入依赖
-         *         <dependency>
-         *             <groupId>com.baomidou</groupId>
-         *             <artifactId>mybatis-plus-boot-starter</artifactId>
-         *             <version>3.2.0</version>
-         *         </dependency>
+ *                 <dependency>
+ *                     <groupId>com.baomidou</groupId>
+ *                     <artifactId>mybatis-plus-boot-starter</artifactId>
+ *                     <version>3.2.0</version>
+ *                 </dependency>
  *                  2）配置
  *                  i.配置数据源
  *                      -导入数据库驱动
@@ -20,6 +20,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *                  ii.配置MyBatis-Plus
  *                      -使用@MapperScan
  *                      -告诉MyBatis-Plus，sql映射文件位置
+ *              2、逻辑删除
+ *                  1）配置全局的逻辑删除规则（省略）
+ *                  2）配置逻辑删除的组件Bean（3.1.1开始可以省略）
+ *                  3）给Bean字段加上逻辑删除注解@TableLogic
  * @author tan
  * @date 2020/10/10 21:32
  * @param null
--- mall-product/src/main/java/com/tan/mall/product/controller/CategoryController.java
+++ mall-product/src/main/java/com/tan/mall/product/controller/CategoryController.java
@@ -50,7 +50,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -61,6 +61,15 @@ public class CategoryController {
     public R save(@RequestBody CategoryEntity category){
 		categoryService.save(category);
 
+        return R.ok();
+    }
+
+    /**
+     * 批量修改
+     */
+    @RequestMapping("/update/sort")
+    public R updateSort(@RequestBody CategoryEntity[] categories){
+        categoryService.updateBatchById(Arrays.asList(categories));
         return R.ok();
     }
 
@@ -77,12 +86,14 @@ public class CategoryController {
 
     /**
      * 删除
+     * @RequestBody 获取请求体，必须发送POST请求
+     * SpringMVC自动将请求体的数据（json）转为对应的对象
      */
     @RequestMapping("/delete")
     //@RequiresPermissions("product:category:delete")
     public R delete(@RequestBody Long[] catIds){
-		categoryService.removeByIds(Arrays.asList(catIds));
-
+		//categoryService.removeByIds(Arrays.asList(catIds));
+        categoryService.removeMenuByIds(Arrays.asList(catIds));
         return R.ok();
     }
 
